e4a15a6e3230a17a09ee26cab45541da4a36d393
Sameer-Shahzada <md.shahzada1606@gmail.com>
2026-04-08 14:28:44 +0530

    tours list

---
+++ src/components/Tour.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const Tour = ({ id, name, info, image, price, removeTour }) => {
+
+  const [readMore, setReadMore] = useState(false);
+
+  return (
+    <div className="tour-card">
+      <img src={image} alt={name} />
+
+      <h3>{name}</h3>
+      <h4>${price}</h4>
+
+      <p>
+        {readMore ? info : `${info.substring(0, 200)}...`}
+        <button onClick={() => setReadMore(!readMore)}>
+          {readMore ? "See Less" : "Show More"}
+        </button>
+      </p>
+
+      <button onClick={() => removeTour(id)}>
+        Remove
+      </button>
+    </div>
+  );
+};
+
+export default Tour;
