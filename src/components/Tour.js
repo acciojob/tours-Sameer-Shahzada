c5bae6c8e5821e7cf6536f646186e0a07e40d0f0
Sameer-Shahzada <md.shahzada1606@gmail.com>
2026-04-08 15:18:26 +0530

    correction - Show less

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -19,7 +19,7 @@ const Tour = ({ id, name, info, image, price, removeTour }) => {
                     id={`see-more-${id}`}
                     onClick={() => setReadMore(!readMore)}
                 >
-                    {readMore ? "See Less" : "See more"}
+                    {readMore ? "Show less" : "See more"}
                 </button>
             </p>
             <button
